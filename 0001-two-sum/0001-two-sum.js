6101864d004e0b28793ca7a0cff7bd25857926b0
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-09-19 21:57:19 +0900

    Time: 63 ms (63.55%), Space: 44.7 MB (10.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,13 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
-// 투포인터해도 되고 -> 정렬이 안되어 있어서 안됨
-// 걍 이중 반복문 돌려도 되고..
+
+// 1. 첫번째 회문에 해쉬 맵에 모두 값을 넣는다
+// 2. 두번째 회문에 target - nums[i]의 값이 해쉬맵에 존재하는지 찾는다. //해쉬맵에서 값찾는 것은 O(1)
+// 3. 있으면 반환한다.
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i+1; j < nums.length; j++) {
-            if(nums[i]+nums[j] === target) return [i,j];
-        }
-    }
+    const hash = new Map();
+    nums.forEach((num, i)=> hash.set(num, i));
     
+    for(let i = 0; i < nums.length; i++) {
+        const targetInNums = target - nums[i];
+        const isTargetIdxArr = hash.has(targetInNums) && hash.get(targetInNums) !== i;
+        
+        if(isTargetIdxArr) return [i, hash.get(targetInNums)];
+    }
 };
