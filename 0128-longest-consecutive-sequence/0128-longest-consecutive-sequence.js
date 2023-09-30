dca7aa3a3200896f10a04f12e90a8ca2c52ac387
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-09-30 20:51:17 +0900

    Time: 1410 ms (5.03%), Space: 64.7 MB (28.63%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -2,27 +2,28 @@
  * @param {number[]} nums
  * @return {number}
  */
-//흐으음.. You must write an algorithm that runs in O(n) time.
-// 1번 회문으로 set에 다 넣음
-    // 2번 회문으로 Min 찾음
-    // 3번 회문으로 min +1씩하면.. 안되겠넹 (1 345)면 안됨
-// map으로 각 원소별 consecutive를 구한다해도 두번 돌아야하는데..
-// 가장 쉬운방법은 정렬때리면 됨.
-
 var longestConsecutive = function(nums) {
-    if(!nums.length) return 0;
-    nums = [...new Set(nums.sort((a,b) => a-b))];
-    let len = 1,
-        answer = 1;
+    const set = new Set();
     
-    for(let i = 0; i < nums.length - 1; i++) {
-        const curr = nums[i];
+    nums.forEach((num) => set.add(num));
+    
+    let longestStreak = 0;
+    
+    for(let i = 0; i < nums.length; i++) { 
+        const num = nums[i];
         
-        if(curr + 1 === nums[i+1]) len += 1;  
-        else len = 1;
-        
-        answer = Math.max(answer, len);
+        if(!set.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+            
+            while(set.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+            
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
     }
     
-    return answer;
+    return longestStreak;
 };
