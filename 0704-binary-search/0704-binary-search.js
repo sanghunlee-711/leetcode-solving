a5f1a7338006231f1c684d357308ebbcbb2cd516
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-10-18 15:11:48 +0900

    Time: 60 ms (29.43%), Space: 44.7 MB (75.36%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -3,17 +3,18 @@
  * @param {number} target
  * @return {number}
  */
+// Assertion way : Find optimized assertion position and check value existing
 var search = function(nums, target) {
-    let start = 0, end = nums.length - 1;
+    let start = 0, end = nums.length;
     
-    while(start <= end) {
+    while(start < end) {
         const mid = Math.floor((start + end) / 2);
         
-        if(nums[mid] === target) return mid;
-        
-        if(nums[mid] < target) start = mid + 1;
-        if(nums[mid] > target) end = mid - 1;
+        if(nums[mid] <= target) start = mid+1;
+        if(nums[mid] > target) end = mid;
     }
     
+    if(start > 0 && nums[start - 1] === target) return start - 1;
+
     return -1;
 };
