239bc1707174589c6741e2757bca61bea3033634
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-10-24 22:09:16 +0900

    Time: 93 ms (42.21%), Space: 53.2 MB (81.53%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,25 +2,27 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// L -> L[i] = L[i-1] * nums[i-1]; // i > 0
+// R -> R[i] = R[i+1] * nums[i+1]; // i < nums.length - 1;
 var productExceptSelf = function(nums) {
     const length = nums.length;
+    const L = Array.from({length}, () => null);
+    const R = Array.from({length}, () => null);
+    const answer = Array.from({length}, () => null);
     
-    const left = Array.from({length}); //해당 인덱스 이전의 좌측 원소의 모든 곱의 값을 나타냄
-    const right = Array.from({length}); // i인덱스 제외 오른쪽 모든 원소의 값의 곱을 나타냄
-    const answer = Array.from({length}); //이러면 해당 인덱스에서는 L[i] * R[i]를 하게 되면 됨
+    L[0] = 1;
+    R[length - 1] = 1;
     
-    left[0] = 1, right[length - 1] = 1;
-    
-    for(let i = 1; i < length; i++){
-        left[i] = left[i-1] * nums[i-1];
+    for(let i = 1; i < nums.length; i++) {
+        L[i] = L[i-1] * nums[i-1];
     }
     
     for(let i = length - 2; i >= 0; i--) {
-        right[i] = right[i+1] * nums[i+1];
+        R[i] = R[i+1] * nums[i+1];
     }
     
-    for(let i = 0; i < length; i++) {
-        answer[i] = left[i] * right[i];
+    for(let i = 0; i < nums.length; i++) {
+        answer[i] = L[i] * R[i];
     }
     
     return answer;
