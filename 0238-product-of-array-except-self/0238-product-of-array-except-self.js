cc25c4b3097d953def8a4762aeec6fc36174fc29
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-10-24 22:17:58 +0900

    Time: 85 ms (69.15%), Space: 53 MB (85.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,27 +2,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-// L -> L[i] = L[i-1] * nums[i-1]; // i > 0
-// R -> R[i] = R[i+1] * nums[i+1]; // i < nums.length - 1;
 var productExceptSelf = function(nums) {
     const length = nums.length;
-    const L = Array.from({length}, () => null);
-    const R = Array.from({length}, () => null);
     const answer = Array.from({length}, () => null);
     
-    L[0] = 1;
-    R[length - 1] = 1;
+    answer[0] = 1;
     
-    for(let i = 1; i < nums.length; i++) {
-        L[i] = L[i-1] * nums[i-1];
+    //left part multiple
+    for(let i = 1; i < length; i++) {
+        answer[i] = answer[i-1] * nums[i-1];
     }
     
-    for(let i = length - 2; i >= 0; i--) {
-        R[i] = R[i+1] * nums[i+1];
-    }
+    // R array is replaced with variable
+    let R = 1;
     
-    for(let i = 0; i < nums.length; i++) {
-        answer[i] = L[i] * R[i];
+    for(let i = length - 1; i >= 0; i--) {
+        answer[i] = answer[i] * R;
+        R *= nums[i]
     }
     
     return answer;
