ed84f94c4c86a31b926e21c774f70315a45b5f3b
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-10-25 17:06:57 +0900

    Time: 69 ms (15.59%), Space: 44.2 MB (76.65%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 // 포인터 두개 두고.. while문을 돌리면 되긴할 것 같은데 chars자체를 수정해야하는게 난제임.. => 걍 마지막에 회문돌자..
-// ["a","b"] 인 경우 카운터가 애매해지네 ..
+// 다른 사람들의 풀이를 보니 정답 체크 자체가 특정 idx뒤로 chars값을 체크를 하지 않음..
 var compress = function(chars) {
     let start = 0, 
         end = 0;
@@ -12,11 +12,13 @@ var compress = function(chars) {
         let count = 0;
         let curr = chars[end];
         
+        //변경되는지 확인
         while(chars[end] === curr) {
             end++;
             count++;
         }
         
+        //오른 카운터 만큼 값 chars에 반영
         chars[start] = curr;
         start++;
         
@@ -28,6 +30,6 @@ var compress = function(chars) {
         }
     }
     
-    
+    //start가 변경된 문자열의 길이
     return start;
 };
