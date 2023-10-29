3f106ca4b45e27c2d4f2bf9ce50db721e4fbb963
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-10-29 19:39:23 +0900

    Time: 78 ms (11.85%), Space: 49.5 MB (60.85%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -2,29 +2,23 @@
  * @param {number[]} height
  * @return {number}
  */
-
-// 두번 돌리면 쉬울텐데 .. -> TLE
-// 정렬은 불가하고
-// 투포인터로 하려면 조건이 필요한데 .. target이 없음
-// 더 짧은 vertical의 포인터를 이동한다면 y-axis가 줄어들더라도 전체 너비가 상승할 수 있는 여지가 있기에 이동시키는 것이 유리함.
-
 var maxArea = function(height) {
-    let answer = 0,
-        start = 0,
-        end = height.length - 1;
+    let start = 0,
+        end = height.length - 1,
+        max = -Infinity;
     
     while(start < end) {
-        const vertical = Math.min(height[start], height[end]);
-        const square = (end - start) * (vertical);
+        const x = end - start;
+        const y = Math.min(height[end], height[start]);
+        const area = x * y;
+        max = Math.max(max, area);
         
-              answer = Math.max(answer, square);
-        
-        if(height[start] <= height[end]) {
+        if(height[start] < height[end]) {
             start++;
-        } else {
-            end--
+        }else {
+            end--;
         }
     }
     
-    return answer;
+    return max;
 };
