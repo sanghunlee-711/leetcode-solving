1a4b5bbd53c42ac8ae50509376e7d8a8fec730ba
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-10-30 13:45:11 +0900

    Time: 144 ms (55.22%), Space: 53.2 MB (64.31%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+// BF -> 회문 중첩으로 돌리면 간단하게 해결?
+// var maxOperations = function(nums, k) {
+//     let count = 0;
+    
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = i+1; j < nums.length; j++) {
+//             if(nums[i] === null || nums[j] === null) continue;
+//             const sum = nums[i] + nums[j];
+            
+//             if(sum === k) {
+//                 nums[i] = null;
+//                 nums[j] = null;
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// };
+
+//6 | 1 null 3 null 4
+// 위 BF를 정렬후 투포인터를 활용한다면?
+var maxOperations = function(nums, k) {
+    let count = 0;
+    nums = nums.sort((a,b) => a-b);
+    let start = 0, end = nums.length - 1;
+    
+    while(start < end) {
+        if(nums[start] === null) {
+            start++;
+        }
+        
+        if(nums[end] === null){
+            end--;
+        }
+        
+        if(nums[start] + nums[end] === k) {    
+            count++;
+            nums[end] = null;
+            nums[start] = null;
+            start++;
+            end--;
+        } else if(nums[start] + nums[end] > k){
+            end--;
+        } else if(nums[start] + nums[end] < k) {
+            start++;
+        }
+    }
+    
+    return count;
+};
+
+
