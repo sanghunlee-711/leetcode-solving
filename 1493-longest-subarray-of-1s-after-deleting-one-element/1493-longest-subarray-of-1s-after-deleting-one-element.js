39f17f31a9495676c1f6300bb2e18913b3008c5a
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-11-03 14:47:02 +0900

    Time: 57 ms (80.42%), Space: 46.7 MB (94.44%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 윈도우의 크기가 늘어난 뒤에 줄 필요가 있는가? -> X 
+// 1개의 0을 삭제했을때 최대길이가 된 경우에서 윈도우의 start, end를 이동시키면 될 것 같다.
+// 
+
+
+var longestSubarray = function(nums) {
+    let start = 0, end = 0;
+    let deleteCount = 0;
+    let max = 0;
+    
+    while(end < nums.length) {
+        if(nums[end] === 0 && deleteCount === 0) {
+            deleteCount = 1;
+            max = Math.max(max, end - start + 1 - deleteCount);
+            end++;
+            continue;
+        }
+        
+        if(nums[end] === 0 && deleteCount === 1) {
+            if(nums[start] === 0) deleteCount = 0;
+            start++;
+            continue;
+        }
+        
+        max = Math.max(max, end - start + 1 - deleteCount);
+        end++;
+    }
+    
+    
+    return max === nums.length ? max - 1 : max;
+};
