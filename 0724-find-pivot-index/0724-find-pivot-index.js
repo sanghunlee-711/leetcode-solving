64629059c79331fb24b2e2a965f624b9ff0dd04e
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-11-06 13:21:49 +0900

    Time: 59 ms (79.95%), Space: 45 MB (27.06%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -2,28 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-// 왼쪽에서 누적합 배열과 오른쪽에서 누적합배열에 동일한 인덱스가 있으면 해당 인덱스 반환해주면 됨.
 var pivotIndex = function(nums) {
-    let sumLeft = Array.from({length: nums.length},() => null);
-    let sumRight = Array.from({length: nums.length},() => null);
-    let prevSum = 0;
-    
-    for(let i = 0; i < nums.length; i++){
-        sumLeft[i] = prevSum;
-        prevSum += nums[i];
-    }
+    let sum = nums.reduce((acc,num)=>{
+        acc += num
+        return acc;
+    },0),
+        leftSum = 0;
     
     
-    prevSum = 0;
-    
-    for(let i = nums.length - 1; i >= 0; i--){
-        sumRight[i] = prevSum;
-        prevSum += nums[i];
-    }
-    
-    for(let i = 0; i < nums.length; i++){
-        if(sumLeft[i] === sumRight[i]) return i;
-        
+    for(let i = 0; i < nums.length; i++) {
+        if(leftSum === sum - leftSum - nums[i]) return i;
+        leftSum += nums[i];
     }
     
     return -1;
