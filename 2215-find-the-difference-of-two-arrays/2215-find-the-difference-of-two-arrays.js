75f9846f80886ec4f460ec3037091ac4ff3cbcd8
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-11-06 13:37:25 +0900

    Time: 79 ms (72.31%), Space: 49.3 MB (22.86%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -3,21 +3,17 @@
  * @param {number[]} nums2
  * @return {number[][]}
  */
+
 var findDifference = function(nums1, nums2) {
-    const set1 = new Set(nums1),
-          set2 = new Set(nums2);
+    nums1 = new Set(nums1);
+    nums2 = new Set(nums2);
     
-    let arr1 =[],
-          arr2 = [];
-    
-    for(let i = 0; i < nums2.length; i++) {
-        if(!set1.has(nums2[i])) arr2.push(nums2[i]);
+    for(let num1 of nums1) {
+        if(nums2.has(num1)) {
+            nums1.delete(num1);
+            nums2.delete(num1);
+        }
     }
     
-    for(let i = 0; i < nums1.length; i++) {
-        if(!set2.has(nums1[i])) arr1.push(nums1[i]);
-    }
-    arr1 = [...new Set(arr1)],
-    arr2 = [...new Set(arr2)]
-    return [arr1,arr2]
+    return [Array.from(nums1), Array.from(nums2)];
 };
