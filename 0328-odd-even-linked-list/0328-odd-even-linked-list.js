94c914b53a415bb1e2b4118129214ef6d283d9a3
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-11-27 10:54:24 +0900

    Time: 54 ms (91.75%), Space: 44.8 MB (30.11%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.js
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -9,23 +9,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-// event head , odd head를 만들고 
-// event head를 제일 뒤에 붙여주면 됨
 var oddEvenList = function(head) {
+    //edge case
     if(!head) return null;
     
-    let odd = head,
-        even = head.next,
-        evenHead = even;
+    let odd = head;
+    let even = head.next;
+    let evenHead = even;
     
     while(even && even.next) {
-        odd.next = even.next; //even을 별도로 빼기위해 관계성 없앰
+        odd.next = even.next;
         odd = odd.next;
-        even.next = odd.next; //even을 기존리스트에서 빼냄
-        
+        even.next = odd.next;
         even = even.next;
     }
-    odd.next = evenHead;
     
+    odd.next = evenHead;
     return head;
 };
