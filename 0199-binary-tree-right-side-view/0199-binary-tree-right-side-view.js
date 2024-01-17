1282d83734116857f3c1ea37de60b640a84e48a4
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2024-01-17 22:44:42 +0900

    Time: 52 ms (77.30%), Space: 43.3 MB (90.64%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -10,25 +10,26 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-
-//Bfs로 탐색하는데.. 제일 오른쪽만 남기면 된다.
-var rightSideView = function(root) { 
-    //base
+var rightSideView = function(root) {
     if(!root) return [];
-    let queue = [root], 
-        rightSideNode = [];
+    
+    const queue = [root];
+    const result = [];
     
     while(queue.length > 0) {
-        const thisLevelNodeLength = queue.length;
+        const currLen = queue.length;
         
-        for(let i = 0; i < thisLevelNodeLength; i++) {
-            let node = queue.shift();
-            console.log(node.val);
-            if(i === thisLevelNodeLength - 1) rightSideNode.push(node.val);
+        for(let i = 0; i < currLen; i++) {
+            const node = queue.shift(); //currNode;
+            
+            //the right === i === currLen - 1;
+            if(i === currLen - 1) result.push(node.val);
             if(node.left) queue.push(node.left);
             if(node.right) queue.push(node.right);
         }
+            
+    
     }
     
-    return rightSideNode;
+    return result;
 };
