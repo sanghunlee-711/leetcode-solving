ac0c878f8c9ff8710de8befdf236dd2a3f3f87f2
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2024-02-09 15:29:42 +0900

    Time: 58 ms (82.30%), Space: 51.4 MB (28.54%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.js
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -2,31 +2,31 @@
  * @param {number[][]} isConnected
  * @return {number}
  */
-//check every node is visited for knowing province(that newly visitied);
-
-const dfs = (isConnected, i, visit) => {
+function dfs (nodes, i, visit) {
     visit[i] = true;
     
-    for(let j = 0; j < isConnected.length; j++){
-        if(isConnected[i][j] === 1 && visit[j] === false) {
-            dfs(isConnected, j, visit);
+    for(let j = 0; j < nodes.length; j++) {
+        //connected and not-visited
+        if(nodes[i][j] === 1 && !visit[j]) {
+            dfs(nodes, j, visit);
         }
     }
-    
 }
 
+
 var findCircleNum = function(isConnected) {
-    const len = isConnected.length,
-          visit = Array.from({length:len},()=>false); //initialize all the visit state is false;
-    let res = 0;
+  const len = isConnected.length;
+  const visit = {};
+    let count = 0;
     
     for(let i = 0; i < len; i++) {
-        if(!visit[i]){
-            //count up if not visited case find
-            res+=1;
-            dfs(isConnected, i, visit)
+        if(!visit[i]) {
+            //when not visit => it may another group(provinces)
+            count+=1;
+            dfs(isConnected, i, visit);
         }
     }
     
-    return res;
+    
+    return count;
 };
