46850447122c2aeb2afcd8aa503dc64ebe30ef0a
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2024-02-16 23:35:02 +0900

    Time: 66 ms (72.96%), Space: 55.4 MB (14.90%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -2,72 +2,63 @@
  * @param {number[][]} grid
  * @return {number}
  */
-/*
-definitely bfs with graph problem
-In each time move to adjacent graph and check
-and check
-after finishing traversal, if there is still 1 orange
-return value'd be 'count'
-otherwise, return -1
-
-처음에 2인것부터 먼저 찾아야하나 -> 
-1. 2인 포지션 찾기 -> queue에 넣기 , 1인거 카운트하기
-2. 2인 곳에서 부터 bfs?
-그럼 충돌하면 어케되냐
-방문처리는 'visit'으로 한다
-*/
 var orangesRotting = function(grid) {
     const queue = [];
-    const direction = [[1,0],[0,1],[-1,0],[0,-1]];
+    const dir = [[1,0],[0,1],[-1,0],[0,-1]];
     const rowLen = grid.length;
     const colLen = grid[0].length;
-    //1. 2인포지션 먼저 찾기
-    let freshOrange = 0;
+    let freshOranges = 0,
+        minutes = 0;
     
-    for(let row = 0; row < rowLen; row++){
+    //1. 썩은 오렌지 포지션 잡기
+    for(let row = 0; row < rowLen; row++) {
         for(let col = 0; col < colLen; col++) {
             if(grid[row][col] === 2) queue.push([row, col]);
-            if(grid[row][col] === 1) freshOrange+=1;
+            if(grid[row][col] === 1) freshOranges++;
         }
     }
     
-    queue.push(['visit','visit']);
-    
-    let count = 0;
-    
-    //2. bfs (with 방문처리)
+    //base case
+    if(freshOranges === 0) return 0;
+
+    //3. BFS
     while(queue.length > 0) {
-        const curr = queue.shift();
-        const [currRow, currCol] = curr;
+        const currQlen = queue.length;
+        let rottenOranges = 0;
         
-        //이게 표시된거면 전단계에서 이미 방문된 것이므로 절차상 카운트 + 1 및 queue가 비어있지 않다면 방문처리를 위해 visit 주입
-        if(currRow === 'visit') {
-            count += 1; //프로세스 종료
+        for(let i = 0; i < currQlen; i++) {
+            const curr = queue.shift();
+            const [currRow, currCol] = curr;
             
-            if(queue.length > 0) {
-                queue.push(['visit', 'visit']);
+            if(grid[currRow][currCol] === 1) {
+                    grid[currRow][currCol] = 2;
+                    rottenOranges++;
             }
-            
-            continue;
+        
+            // 4.사방으로 진행
+            for(const [x,y] of dir) {
+                const nextRow = currRow + x;
+                const nextCol = currCol + y;
+
+                const isNext = nextRow >= 0 
+                            && nextCol >= 0
+                            && nextRow < rowLen
+                            && nextCol < colLen;
+                        
+
+                if(isNext && grid[nextRow][nextCol] === 1) {
+                        queue.push([nextRow, nextCol]);
+                }
+
+            }
         }
         
-        //go 4direction
-        for(const [x, y] of direction) {
-            const nextRow = currRow + x;
-            const nextCol = currCol + y;
-            
-            const isNext = nextRow >= 0 && nextCol >=0 && nextRow < rowLen && nextCol < colLen;
-            
-            if(isNext) {
-                if(grid[nextRow][nextCol] === 1) {
-                    grid[nextRow][nextCol] = 2;
-                    freshOrange-=1;
-                    queue.push([nextRow, nextCol]);
-                }    
-            }
-        }
+        if(rottenOranges > 0) {
+            freshOranges -= rottenOranges;
+            minutes++;
+        }    
+      
     }
     
-    
-    return freshOrange === 0 ? count-1 : -1;
+    return freshOranges === 0 ? minutes : -1;
 };
