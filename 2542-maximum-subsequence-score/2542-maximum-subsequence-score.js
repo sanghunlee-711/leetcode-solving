ac63dca4ff242b64a5635808373ebfacaa16c8dc
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2024-02-21 23:03:17 +0900

    Time: 332 ms (72.26%), Space: 89.6 MB (55.48%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.js
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.js
@@ -4,19 +4,9 @@
  * @param {number} k
  * @return {number}
  */
-/*
-그니까 k만큼의 개수를 무작위로 골라서 nums1에서는 k개의 합 
-nums2에서는 min 값을구하고 그 두가지 곱한 것 중 가장 큰값을 알려달라 인 건데 ㅣ..
-nums2를 우선순위로 하는 기준으로 nums1을 정렬하면?? -> ???... 더하기는 어케할건데요
-nums1 = [2,1], nums2 = [5,1], k = 1
-nums1 = [3,1,4,1,2,1], nums2 = [10,9,7,6,5,1]
-1. nums2기준으로 nums1을 정렬한다.
-2. nums2에서 하나의 값이 정해졌다면 그것을 min값으로 가정한다.
-2.1 그러면 nums1에서는 nums2에서 정해진 인덱스와 동일한 값을 제외하고 나머지 2가지중 가장 큰값을 찾으면 된다.
-*/
 class CustomMinHeap {
     constructor() {
-        this.queue = [];
+        this.queue = [];        
     }
     
     size() {
@@ -30,11 +20,10 @@ class CustomMinHeap {
     
     dequeue() {
         if(this.size() === 0) return 0;
-        if(this.size() === 1) {
-            return this.queue.pop();
-        }
+        if(this.size() === 1) return this.queue.pop();
         
         const min = this.queue[0];
+        
         this.queue[0] = this.queue.pop();
         this.sinkDown(0);
         
@@ -43,60 +32,65 @@ class CustomMinHeap {
     
     bubbleUp(idx) {
         
-        
         while(idx > 0) {
             const parentIdx = Math.floor((idx - 1) / 2);
+            
             const isSwap = this.queue[parentIdx] > this.queue[idx];
             
             if(!isSwap) break;
-            //swap;
-            [this.queue[idx], this.queue[parentIdx]] = [this.queue[parentIdx], this.queue[idx]]; 
-            idx = parentIdx;
+            [this.queue[idx], this.queue[parentIdx]] = [this.queue[parentIdx], this.queue[idx]]
+            idx = parentIdx
         }
     }
     
     sinkDown(idx) {
-        const leftChildIdx = idx * 2 + 1,
-              rightChildIdx = idx * 2 + 2;
+        const leftChildIdx = idx * 2 + 1;
+        const rightChildIdx = idx * 2 + 2;
         let minIdx = idx;
         
-        const isLeftSwap = leftChildIdx < this.size()
+        const isLeftSwap = minIdx < this.size()
                         && this.queue[minIdx] > this.queue[leftChildIdx];
         
-        if(isLeftSwap) minIdx = leftChildIdx;
+        if (isLeftSwap) minIdx = leftChildIdx;
         
-        const isRightSwap = rightChildIdx < this.size()
-                && this.queue[minIdx] > this.queue[rightChildIdx];
+        const isRightSwap = minIdx < this.size()
+                        && this.queue[minIdx] > this.queue[rightChildIdx];
         
-        if(isRightSwap) minIdx = rightChildIdx;
+        if (isRightSwap) minIdx = rightChildIdx;
+        
         
         const isSwap = minIdx !== idx;
         
         if(isSwap) {
-            [this.queue[minIdx], this.queue[idx]] = [this.queue[idx], this.queue[minIdx]];
+            [this.queue[idx], this.queue[minIdx]] = [this.queue[minIdx], this.queue[idx]]
             this.sinkDown(minIdx);
         }
     }
+    
+    
 }
 
 var maxScore = function(nums1, nums2, k) {
-        const pairs = Array.from({length: nums1.length}, (_, idx) => [nums1[idx], nums2[idx]]).sort((a,b)=> b[1] - a[1]);
-        const topKHeap = new CustomMinHeap();
-        let topKSum = 0;
-        
-        for(let i = 0; i < k; i++) {
-            topKSum += pairs[i][0];
-            topKHeap.enqueue(pairs[i][0]);
-        }
+    //nums2기준으로 내림차순 정렬
+    const len = nums1.length;
+    const pairs = Array.from({length: len}, (_,idx)=> [nums1[idx], nums2[idx]]).sort((a,b)=> b[1] - a[1]);
+    const minHeap = new CustomMinHeap();
+    let topKSum = 0;
     
-        let answer = topKSum * pairs[k-1][1];
-        
+    //k-1 까지만 회문을 돌며 minHeap을 활용할 것임
+    for(let i = 0; i < k; i++) {
+        topKSum += pairs[i][0];            
+        minHeap.enqueue(pairs[i][0]);
+    }
     
-        for(let i = k; i < nums1.length; i++) {
-            topKSum += pairs[i][0] - topKHeap.dequeue();
-            topKHeap.enqueue(pairs[i][0]);
-            answer = Math.max(answer, topKSum * pairs[i][1]);
-        }
+    let answer = topKSum * pairs[k-1][1];
     
-        return answer
+    // k부터 하나씩 minHeap으로 부터 가져오는 최소의 값을 빼가며 topKSum을 항상 최대상태로 유지
+    for(let i = k; i < len; i++) {
+        topKSum += pairs[i][0] - minHeap.dequeue();
+        minHeap.enqueue(pairs[i][0])
+        answer = Math.max(answer, topKSum * pairs[i][1])
+    }
+    
+    return answer;
 };
