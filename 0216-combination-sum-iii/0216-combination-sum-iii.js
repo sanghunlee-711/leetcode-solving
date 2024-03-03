e885e184788109546cd15949500042ac08566773
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2024-03-03 22:04:19 +0900

    Time: 56 ms (43.59%), Space: 50.4 MB (20.56%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} comb
+ * @param {number} k
+ * @param {number[]} combinations
+ * @return void
+ */
+const backtrack = (comb, k, combinations, remain, start) => {
+    // base case
+    if(comb.length === k && remain === 0) return combinations.push(comb);
+    else if(remain < 0 || comb.length === k) return; //exceed scope;
+    
+    for(let i = start; i < 9; i++) {
+        const nextComb = [...comb, i+1],
+              nextRemain = remain - i - 1,
+              nextStart = i + 1;
+        backtrack(nextComb, k, combinations, nextRemain, nextStart);
+    }
+}
+
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function(k, n) {
+    const combinations = [];
+    
+    backtrack([], k, combinations, n, 0);
+    
+    return combinations;
+};
