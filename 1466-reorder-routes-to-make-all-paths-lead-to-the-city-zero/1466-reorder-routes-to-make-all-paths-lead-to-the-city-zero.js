6b3d277d366f452f4f0c3d3e9794664694e67d62
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2024-07-07 07:02:34 +0900

    Time: 312 ms (93.31%), Space: 90.8 MB (33.14%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.js
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.js
@@ -3,46 +3,43 @@
  * @param {number[][]} connections
  * @return {number}
  */
-//이건 어케해야할까
-// 다 탐색한다음 0로 향하게 해야할 것 같은데 흠.. 백트래킹
-
-// 0 -> fake (반대방향)
-// 1 -> origin (원래방향)
-
-
-//  순회 중에 "원래" 가장자리, 즉 1로 표시된 가장자리를 발견하면 개수가 1 증가합니다
-// "인공적인" 가장자리를 발견하더라도 개수를 수정하지 않습니다
-//  "인공" = 0 또는 "원본" = 1
-
-
+/**
+ * 1. bfs를 통해 도시 0 에서 시작해 모든 도시에 도달할 경로를 찾자
+ * 2. 1을 통해 확인한 연결된 도로를 통해, 반대방향으로 연결된 도로를 재배치 할 도로로 간주하자.
+*/
 var minReorder = function(n, connections) {
-    const adj = {};
-    const visited = {};
-    let count = 0;
+    const graph = Array.from({length: n}, () => []),
+          reverseGraph = Array.from({length: n}, () => []);
     
-    //set origin as 1
-    //fake as 0
-    for( const [a,b] of connections) {
-        if(!adj[a]) adj[a] = [];
-        if(!adj[b]) adj[b] = [];
-        
-        adj[a].push([b,1]);
-        adj[b].push([a,0]);
+    for(const [u, v] of connections) {
+        graph[u].push(v); // 원래 방향
+        reverseGraph[v].push(u) // 반대 방향
     }
-    const dfs = function (position) {
-        if(visited[position]) return;
-        visited[position] = true;
+    
+    const queue = [0], //0에서 부터 시작하는 큐
+          visited = new Set([0]); // 방문한 도시 확인
+    let reorderCount = 0;
+    
+    while(queue.length) {
+        const curr = queue.shift();
         
-        for(const connection of adj[position]) {
-            // 0 이라면 어차피 카운트 안올라가므로
-            if(!visited[connection[0]]) count += connection[1];
-            
-            dfs(connection[0]);
+        //현재 도시에서 나가는 방향의 도로
+        for(const node of graph[curr]) {
+            if(!visited.has(node)) {
+                visited.add(node) // 방문 표시
+                queue.push(node) // 큐에 추가
+                reorderCount++;
+            }
+        }
+        // 현재 도시로 들어오는 도로(반대방향)
+        for(const node of reverseGraph[curr]) {
+            if(!visited.has(node)) {
+                visited.add(node) // 방문 표시
+                queue.push(node);
+                // 0으로 오는 방향이니 불필요
+            }
         }
     }
     
-    //start from root(0)
-    dfs(0);
-    
-    return count;
+    return reorderCount;
 };
