671d66aa9f5aaac28d9eccf6e93e1d1911fdf75f
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-01-22 09:49:58 +0900

    Time: 0 ms (100%), Space: 49.1 MB (66.48%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,18 +4,14 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    const temp = []
     let j = 0;
+
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] !== val) {
-            temp[j] = nums[i];
+            nums[j] = nums[i];
             j++;
         }
     }
 
-    for(let i = 0; i < nums.length; i++) {
-        nums[i] = temp[i];
-    }
-
-    return temp.length;
+    return j
 };
