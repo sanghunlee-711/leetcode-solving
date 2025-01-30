4ac8756bc89b9cdd7fd08f5ff39ebd4af20a9764
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-01-30 12:08:53 +0900

    Time: 0 ms (100%), Space: 52.2 MB (43.17%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -11,16 +11,14 @@
  */
 var reverseList = function(head) {
     let curr = head,
-        prev = null,
-        next = null;
-    
+        prev = null;
+
     while(curr) {
-        next = curr.next;
+        let temp = curr.next;
         curr.next = prev;
-        
         prev = curr;
-        curr = next;
+        curr = temp;
     }
-    
+
     return prev;
 };
