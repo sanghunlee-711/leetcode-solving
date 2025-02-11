bcc660cb52c00d49fd79b2b65b9f21f4f7613673
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-02-11 21:28:56 +0900

    Time: 144 ms (29.24%), Space: 56.2 MB (51.61%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -8,32 +8,34 @@
 /**
  * @param {ListNode[]} lists
  * @return {ListNode}
- T.C: O(k * n)
+ * Divide and conquere way
+ * T.C O(N*K)
  */
 var mergeKLists = function(lists) {
-    //base
     if(lists.length === 0) return null;
 
-    for(let i = 1; i < lists.length; i++) {
-        const [prev, curr] = [lists[i-1], lists[i]]
-        lists[i] = mergeList(curr, prev);
+    for(let i = 0; i < lists.length; i++) {
+        const [prev, curr] = [lists[i-1], lists[i]];
+
+        const mergedList = mergeLists(prev, curr);
+        lists[i] = mergedList;
     }
 
     return lists[lists.length - 1];
 };
 
 
-var mergeList = function(l1, l2) {
+var mergeLists = function(l1, l2) {
     const dummy = new ListNode();
     let tail = dummy;
 
     while(l1 && l2) {
         if(l1.val < l2.val) {
-            tail.next = l1;
-            l1 = l1.next;
-        } else {
-            tail.next = l2;
-            l2 = l2.next;
+            tail.next = l1
+            l1 = l1.next
+        }else {
+            tail.next = l2
+            l2 = l2.next
         }
         tail = tail.next;
     }
@@ -41,5 +43,5 @@ var mergeList = function(l1, l2) {
     if(l1) tail.next = l1;
     if(l2) tail.next = l2;
 
-    return dummy.next;
+    return dummy.next
 }
