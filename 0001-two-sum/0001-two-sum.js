345ac866f6007ac54e0aa5bdf772dfb36fd2c126
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-03-06 21:51:16 +0900

    Time: 1 ms (89.71%), Space: 55.1 MB (12.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -2,19 +2,16 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
+ // 해쉬로 값을 흠..
+ // curr의 마이너스값을 해쉬에서 가지고 있다고 보고 그 idx를 가져온다면
  */
-
-// 1. 첫번째 회문에 해쉬 맵에 모두 값을 넣는다
-// 2. 두번째 회문에 target - nums[i]의 값이 해쉬맵에 존재하는지 찾는다. //해쉬맵에서 값찾는 것은 O(1)
-// 3. 있으면 반환한다.
 var twoSum = function(nums, target) {
-    const hash = new Map();
-    nums.forEach((num, i)=> hash.set(num, i));
+    const map = new Map();
     
     for(let i = 0; i < nums.length; i++) {
-        const targetInNums = target - nums[i];
-        const isTargetIdxArr = hash.has(targetInNums) && hash.get(targetInNums) !== i;
-        
-        if(isTargetIdxArr) return [i, hash.get(targetInNums)];
+        const rest = target - nums[i];
+        if(map.has(rest)) return [map.get(rest), i]
+
+        map.set(nums[i], i)
     }
 };
