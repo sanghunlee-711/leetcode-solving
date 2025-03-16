1b12bd327d23f39c76ed231a8a70da9b982e92b5
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-03-16 18:34:43 +0900

    Time: 12 ms (26.56%), Space: 61.2 MB (34.33%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -1,20 +1,25 @@
 /**
  * @param {number} n
  * @return {number[]}
+ //n개 길이까지 회문을 돌릴건데, 10진수에 해당하는 인덱스 별로 1의 개수를 각 인덱스에 담아 배열로 반환해라.
  */
-const popCount = x => {
+function checkBit(n) {
     let count = 0;
-    
-    for(count = 0; x != 0; count++) {
-        x &= x - 1;
+    while(n > 0) {
+        if(n & 1 === 1) {
+            count += 1;
+        }
+        n = n >> 1;
     }
     return count;
 }
 
 var countBits = function(n) {
-    const ans = Array.from({length: n + 1}, () => 0) ;
-    for(let i = 0; i <= n; ++i) {
-        ans[i] = popCount(i);
+    const ans = Array.from({length: n}, () => 0);
+
+    for(let i = 0; i <= n; i++) {
+        ans[i] = checkBit(i);
     }
+
     return ans;
 };
