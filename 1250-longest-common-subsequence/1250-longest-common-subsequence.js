4fb463efdb077ee43adc69cad30ade446cf8c23c
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-03-20 13:07:44 +0900

    Time: 173 ms (11.93%), Space: 82.2 MB (41.07%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.js
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.js
@@ -2,30 +2,49 @@
  * @param {string} text1
  * @param {string} text2
  * @return {number}
- //2차원 dp구조 생각하면 됨
- 동일한 문자인 경우 대각 오른쪽 아래
- 그렇지 않은경우 우측하나 아래측 하나 진행하면 됨
+ tabulation-> ✅
+
  */
-var longestCommonSubsequence = function(text1, text2) {
-    const ROWS = text1.length,
-        COLS = text2.length;
-    const dp = Array.from({length: ROWS}, () => {
-        return Array.from({length: COLS}, () => -1)
-    });
+// var longestCommonSubsequence = function(text1, text2) {
+//     const ROWS = text1.length, COLS = text2.length;
 
+//     const dp = Array.from({length: text1.length}, () => Array.from({length: text2.length}, () => -1))
 
-    function dfs(i, j) {
-        if(i >= ROWS || j >= COLS) return 0;
-        if(dp[i][j] !== -1) return dp[i][j]
+//     function dfs(i, j) {
+//         if(i >= ROWS || j >= COLS) return 0;
+//         if(dp[i][j] > -1) return dp[i][j];
         
-        if(text1[i] == text2[j]) {
-            dp[i][j] = 1+dfs(i+1, j+1);
-        }
+//         if(text1[i] === text2[j]) {
+//             dp[i][j] = dfs(i+1, j+1) + 1
+//         } else {
+//             dp[i][j] = Math.max(dfs(i+1, j), dfs(i, j+1));
+//         }
 
-        if(text1[i] != text2[j]) {
-            dp[i][j] = Math.max(dfs(i+1, j), dfs(i, j+1))
+//         return dp[i][j]
+//     }
+
+//     dfs(0, 0);
+
+//     return dp[0][0]
+// };
+
+/**
+bottomup
+ */
+ var longestCommonSubsequence = function(text1, text2) {
+    const ROWS = text1.length, COLS = text2.length;
+
+    const dp = Array.from({length: ROWS + 1}, () => Array.from({length: COLS+1}, () => 0))
+
+    for(let i = ROWS - 1; i >= 0; i--) {
+        for(let j = COLS -1; j >= 0; j--) {
+            if(text1[i] === text2[j]) {
+                dp[i][j] = 1 + dp[i+1][j+1];
+            } else {
+                dp[i][j] = Math.max(dp[i][j+1], dp[i+1][j]);
+            }
         }
-        return dp[i][j]
     }
-    return dfs(0,0)
+
+    return dp[0][0]
 };
