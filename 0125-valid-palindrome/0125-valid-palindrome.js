29766ddb2dee53981af2c01f35866c435747a6e7
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-04-06 16:04:18 +0900

    Time: 3 ms (85.65%), Space: 57.3 MB (53.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,12 +1,29 @@
 /**
  * @param {string} s
  * @return {boolean}
+ A man, a plan, a canal: Panama 
+ amanaplanacanalpanama 
+ use RegEx
  */
+const isAlphanum = (curr) => {
+    return ((curr >= "a" && curr <= "z") ||
+            (curr >= "A" && curr <= "Z") ||
+            (curr >= "0" && curr <= "9"));
+}
 
 var isPalindrome = function(s) {
-    const validRegex = /[^a-zA-Z0-9]/g;
-    
-    s = s.replaceAll(validRegex, '').toLowerCase();
-    
-    return s === s.split('').reverse().join('');
+    const len = s.length;    
+    let L = 0, R = len - 1;
+
+    while(L < R) {
+        while(!isAlphanum(s[L]) && L < R) L++;
+        while(!isAlphanum(s[R]) && L < R) R--;
+        if(s[L].toLowerCase() !== s[R].toLowerCase()) return false;
+        
+        L++;
+        R--;
+    }
+
+    return true
+
 };
