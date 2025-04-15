a01ab576d7751c8ceb5b30addd403ffff9ea2e10
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-04-15 21:19:27 +0900

    Time: 0 ms (100%), Space: 54.1 MB (81.19%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -1,32 +1,25 @@
 /**
  * @param {number[]} nums
  * @return {number[][]}
-dfs => backtracking
-[1,2,3] => 
-start                    []   
-i = 0              [1]        []
-i = 1       [1,2]        [1]        [2]       []
-i = 2  [1,2,3] [1,2] [1,3] [1]  [2,3] [2]  [3]  [] 
-
  */
 var subsets = function(nums) {
     const res = [];
-    const subsets = [];
 
-    function dfs(i) {
+    const dfs = (i, currSub) => {
         if(i >= nums.length) {
-            const currSubsets = [...subsets];
-            return res.push(currSubsets);
+            return res.push([...currSub])
         }
         
-        //현재 인덱스의 요소를 추가하고 진행한 dfs
-        subsets.push(nums[i]);
-        dfs(i+1);
-        //현재 인덱스의 요소를 추가하지 않고 진행한 dfs
-        subsets.pop();
-        dfs(i+1);
+        const curr = nums[i];
+
+        currSub.push(curr)
+        dfs(i+1, currSub);
+        currSub.pop() //backtracking
+
+        dfs(i+1, currSub);
     }
 
-    dfs(0);
+    dfs(0, []);
+
     return res;
 };
