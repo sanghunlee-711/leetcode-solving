add08524e499753e63927a7dbdae51501426d15c
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-04-18 23:20:34 +0900

    Time: 6 ms (14.79%), Space: 60.1 MB (32.58%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -1,24 +1,22 @@
 /**
  * @param {number[]} nums
  * @return {number[][]}
- 중복 허용한 모든 경우의 수
+ T.C: O(n * n!) //배열 카피가 없으므로
  */
 var permute = function(nums) {
-    let perms = [[]];
+    const res = [];
 
-    for(let num of nums) {
-        let nextPerms = [];
+    function backtracking(start) {
+        //nums를 직접 스왑하며 조작할 것이기에 다 다른 nums가 반환될 예정
+        if(start === nums.length) return res.push([...nums]);
 
-        for(let p of perms) {
-            for(let i = 0; i <= p.length; i++) {
-                const copyP = [...p];
-                copyP.splice(i, 0, num);
-                nextPerms.push(copyP);
-            }
-            
+        for(let i = start; i < nums.length; i++) {
+            [nums[i], nums[start]] = [nums[start], nums[i]] //임의로 i와 start를 변경해봄
+            backtracking(start + 1);
+            [nums[i], nums[start]] = [nums[start], nums[i]] // 원복
         }
-        perms = nextPerms;
     }
 
-    return perms;
+    backtracking(0)
+    return res;
 };
