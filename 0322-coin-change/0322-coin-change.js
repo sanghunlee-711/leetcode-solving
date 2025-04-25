7922364e68db484ccb6be41762d9737add6ad16c
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-04-25 23:23:47 +0900

    Time: 33 ms (75.85%), Space: 59.1 MB (78.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -2,30 +2,48 @@
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
- @description 매번 모든 케이스에 대해 고려 해보기 DFS
+ @description TopDown 매번 모든 케이스에 대해 고려 해보기 DFS -> Bottom Up dp[i] = 금액 i를 만들기 위한 최소 동전 개수
  */
 var coinChange = function(coins, amount) {
-    const memo = new Map();
+    const dp = new Array(amount + 1).fill(Infinity);
+    dp[0] = 0; // 금액 0을 만들기 위한 동전 수는 0개
 
     //코인 count를 리턴할거임
-    function dfs(amount) {
-        if(amount == 0) return 0;
-        if(memo.has(amount)) return memo.get(amount);
-
-        let res = Infinity;
+    for(let i = 1; i <= amount; i++) {
         for(let coin of coins) {
-            if(amount - coin >= 0){
-                const next = 1 + dfs(amount - coin);
-                res = Math.min(res, next);
-            } 
+            if(i - coin >= 0) {
+                dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+            }
         }
-
-        memo.set(amount, res);
-
-        return res;
     }
 
-    const count = dfs(amount);
-
-    return count === Infinity ? -1 : count;
+    return dp[amount] === Infinity ? -1 : dp[amount]
 };
+
+
+// DP Top Down
+// var coinChange = function(coins, amount) {
+//     const memo = new Map();
+
+//     //코인 count를 리턴할거임
+//     function dfs(amount) {
+//         if(amount == 0) return 0;
+//         if(memo.has(amount)) return memo.get(amount);
+
+//         let res = Infinity;
+//         for(let coin of coins) {
+//             if(amount - coin >= 0){
+//                 const next = 1 + dfs(amount - coin);
+//                 res = Math.min(res, next);
+//             } 
+//         }
+
+//         memo.set(amount, res);
+
+//         return res;
+//     }
+
+//     const count = dfs(amount);
+
+//     return count === Infinity ? -1 : count;
+// };
