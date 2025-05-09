f69f6eedd70528caf30b7c3f9c1107a17d3b4044
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-05-09 18:06:53 +0900

    Time: 4 ms (29.64%), Space: 57.6 MB (80.15%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -9,30 +9,31 @@
 /**
  * @param {TreeNode} root
  * @return {number[][]}
+ queue에 레벨별로 넣고, 각 회차마다 res에 값을 넣어줌
+ 처음에 값이 없는 경우도 고려필요
+ T.C: O(N) //모든 노드 탐색
+ S.C: O(H) res 트리의 높이 ?..
  */
 var levelOrder = function(root) {
-    
-    const queue = [],
-        res = [];
-    let level = 0;
+    if (!root) return [];
 
-    if(root !== null) {
-        queue.push(root)
-    };
-
+    const queue = [root];
+    const res = [];
+    let currIdx = 0;
     while(queue.length > 0) {
         const len = queue.length;
 
         for(let i = 0; i < len; i++) {
-            const curr = queue.shift();
-            
-            if(res[level]) res[level].push(curr.val);
-            else res[level] = [curr.val]
+            const curr = queue.shift()
+            if(res[currIdx]) res[currIdx].push(curr.val);
+            else res[currIdx] = [curr.val];
 
             if(curr.left) queue.push(curr.left)
             if(curr.right) queue.push(curr.right)
         }
-        level += 1;
+        
+        currIdx += 1;
     }
+
     return res;
 };
