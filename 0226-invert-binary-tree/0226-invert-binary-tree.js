3c97bffbece62759055f3af33b4ab7135a5f30ce
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-05-14 22:31:31 +0900

    Time: 0 ms (100%), Space: 55.1 MB (5.93%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -9,22 +9,20 @@
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
- left = right 로 재귀로 바꾸면 될 것. ㅏㅌ음
- T.C: O(N)
- S.C: O(h)// 재귀트리 호출 깊이
  */
 var invertTree = function(root) {
-    function invert(node) {
-        if(!node) return 
-            
-        const temp = node.left;
+    const reverse = (node) => {
+        if(!node) return node;
+
+        const tmp = node.left;
         node.left = node.right;
-        node.right = temp;
-        invertTree(node.left)
-        invertTree(node.right)
-        
+        node.right = tmp;
+
+        reverse(node.left);
+        reverse(node.right);
     }
-    invert(root);
+
+    reverse(root);
 
     return root;
 };
