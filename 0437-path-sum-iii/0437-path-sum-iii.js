56ef232c69b043ecedef78ac4e09f8bef5c2b54d
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-05-16 18:33:24 +0900

    Time: 4 ms (78.48%), Space: 60.8 MB (55.21%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.js
+++ 0437-path-sum-iii/0437-path-sum-iii.js
@@ -5,34 +5,33 @@
  *     this.left = (left===undefined ? null : left)
  *     this.right = (right===undefined ? null : right)
  * }
- */ 
+ */
 /**
  * @param {TreeNode} root
  * @param {number} targetSum
  * @return {number}
+ 누가봐도 dfs이긴 한데 + 백트래킹
+ 해시맵의 키: 합의 값 value: 카운트
+ 누적합 활용하면 됨
+
  */
 var pathSum = function(root, targetSum) {
-    if(!root) return 0;
-    let count = 0;
     const map = new Map();
+    let count = 0;
+    map.set(0,1);
     
-    const dfs = (node, sum) => {
-        if(!node) return null;
-        const newSum = node.val + sum;
-        
-        if(newSum === targetSum) count++;
-        if(map.has(newSum - targetSum)) count += map.get(newSum - targetSum);    
-        
-        map.set(newSum, (map.get(newSum) || 0) + 1)
-        
-        if(node.left) dfs(node.left, newSum);
-        if(node.right) dfs(node.right, newSum);
-        
-        //backtracking
-        map.set(newSum, (map.get(newSum) || 0) - 1)
+    function dfs(node, currSum){
+        if(!node) return;
+        currSum += node.val; //현재 값 추가
+        if(map.has(currSum - targetSum)) count += map.get(currSum - targetSum);
+
+        map.set(currSum, (map.get(currSum) || 0) + 1); //합에 대한 카운트 추가
+        if(node.left) dfs(node.left, currSum)
+        if(node.right) dfs(node.right, currSum)
+        map.set(currSum, map.get(currSum) - 1); //경로 독립성 보장을 위함
     }
-    
+
     dfs(root, 0)
-    
-    return count;
+
+    return count
 };
