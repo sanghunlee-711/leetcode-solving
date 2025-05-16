41039ac0a9bc6dabbeeb3e6781e191e5db0fe337
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-05-16 22:53:05 +0900

    Time: 71 ms (25.28%), Space: 130.4 MB (33.84%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -10,27 +10,24 @@
  * @param {number[]} preorder
  * @param {number[]} inorder
  * @return {TreeNode}
+ preorder: root -> left -> right
+ inorder: left -> root -> right
  */
 var buildTree = function(preorder, inorder) {
-    // 3,9,20,15,7
-    //pre: root -> left -> right 
-    //in: left -> root -> right; // n: curr 2*n -1 => right, 2*n+1 => left
-
     if(!preorder.length || !inorder.length) return null;
+
     const rootVal = preorder[0];
     const root = new TreeNode(rootVal);
-    let mid = inorder.indexOf(rootVal);
 
+    const idx = inorder.indexOf(rootVal);
     root.left = buildTree(
-        preorder.slice(1, mid + 1),
-        inorder.slice(0, mid),
-    )
-
+        preorder.slice(1, idx + 1),
+        inorder.slice(0, idx + 1)
+    );
     root.right = buildTree(
-        preorder.slice(mid+1),
-        inorder.slice(mid+1),
-    )
-    
-    
+        preorder.slice(idx+1),
+        inorder.slice(idx+1)
+    );
+
     return root;
 };
