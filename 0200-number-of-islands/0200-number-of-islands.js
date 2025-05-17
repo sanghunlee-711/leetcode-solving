310b67fc7310a4be6e4708ad684a12b028e3b234
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-05-17 12:10:39 +0900

    Time: 80 ms (27.65%), Space: 65.7 MB (27.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -1,34 +1,54 @@
 /**
  * @param {character[][]} grid
  * @return {number}
+ 방문처리를 꼭 한다
+ 전체 회문을 돌면서 bfs를 통해 카운트를 한다.
+ 회문돌때 1인 경우만 bfs를 한다
+ bfs는 0을만나거나 Edge에 해당하는경우 멈춘다.
  */
-var numIslands = function(grid) {
-    const ROWS = grid.length,
-        COLS = grid[0].length;
-    let islandCount = 0;
-    
-    function dfs (row, col, grid) {
-        //범위 벗어나면 return;
-        if(row >= ROWS || col >= COLS || row < 0 || col < 0) return;
-        //0을 마주쳐도 return
-        if(grid[row][col] === '0') return;
-        //방문하면 0 처리
-        grid[row][col] = '0';
-        //사방으로 dfs
-        dfs(row + 1, col, grid)
-        dfs(row - 1, col, grid)
-        dfs(row, col + 1, grid)
-        dfs(row, col - 1, grid)
-    }
+ const directions = [
+    [1,0],
+    [-1,0],
+    [0,-1],
+    [0,1],
+ ]
 
-    for(let r = 0; r < ROWS; r++) {
-        for(let c = 0; c < COLS; c++) {
-            if(grid[r][c] === '1') {
-                islandCount+= 1;
-                dfs(r, c, grid)
-            }   
+var numIslands = function(grid) {
+    if(!grid || !grid.length) return 0;
+
+    const rows = grid.length,
+        cols = grid[0].length;
+    let count = 0;
+    const visited = Array.from({length: rows}, 
+                () => Array.from({length: cols}, 
+                () => false));
+
+    function bfs(r,c) {
+        const queue = [[r,c]];
+        visited[r][c] = true;
+
+        while(queue.length) {
+            const [row, col] = queue.shift();
+            for(const [dx,dy] of directions) {
+                const [nr, nc] = [row + dx, col + dy]
+                if(nr >= 0 && nr < rows &&
+                    nc >= 0 && nc < cols &&
+                    grid[nr][nc] === '1' && !visited[nr][nc]) {
+                        visited[nr][nc] = true;
+                        queue.push([nr, nc])
+                    }
+            }
         }
     }
 
-    return islandCount;
+    for(let r = 0; r < rows; r++) {
+        for(let c = 0; c < cols; c++) {
+            if(grid[r][c] === '1' && !visited[r][c]) {
+                count += 1;
+                bfs(r,c)
+            }
+        }
+    }
+
+    return count;
 };
