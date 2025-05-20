1c86a009b826bc6e3d6046c6e8b3931faa22896c
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-05-20 13:16:13 +0900

    Time: 3 ms (99.71%), Space: 60.9 MB (83.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -2,34 +2,43 @@
  * @param {number} numCourses
  * @param {number[][]} prerequisites
  * @return {boolean}
+ 싸이클이 생기면 안되는 전형적인 Topolical sort
+ 1. adjs List (hash)로 과목별 선수과목을 기재
+ 2. 각 과목을 방문처리하며 재 방문 되는 경우 false로 처리
  */
-
-
 var canFinish = function(numCourses, prerequisites) {
-    const adj = {}
-    const visit = new Set();
-
+    //1
+    const adj = {};
+    const visited = new Set();
     for(let i = 0; i < numCourses; i++) adj[i] = [];
-    for(let [src, dst] of prerequisites) adj[src].push(dst)
-    
-    function dfs (crs) {
-        //cycle
-        if(visit.has(crs)) return false;
-        //더 이상 사전 요구 과목 없음
-        if(adj[crs].length === 0) return true;
-        //방문 처리
-        visit.add(crs);
+    prerequisites.forEach(([course, dst]) => {
+        adj[course].push(dst)
+    })
 
-        for(let pre of adj[crs]) {
-            if(!dfs(pre)) return false;
+    //2
+    function dfs(course) {
+        //cycle이 되는 경우
+        if(visited.has(course)) return false;
+        //이미 수강을 완료한 케이스
+        if(adj[course].length === 0) return true;
+        visited.add(course)
+
+        const pre = adj[course];
+        for(const preCourse of pre) {
+            if(!dfs(preCourse)) return false;
         }
-        //이미 체크 된 경우 빈 값 처리 해준다.
-        visit.delete(crs);
-        adj[crs] = [];
+        //dfs를 통해서도 싸이클이 없다 판단했기에 수강 완료
+        adj[course] = []
+        //백트래킹
+        visited.delete(course)
+
         return true;
+
     }
 
-    for(let i = 0; i < numCourses; i++) if(!dfs(i)) return false;
-    
+    for(let i = 0; i < numCourses; i++) {
+        if(!dfs(i)) return false;
+    }
+
     return true;
 };
