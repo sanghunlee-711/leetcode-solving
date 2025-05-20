38fa562c2b14eba9e4d06794fc7d543253004151
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-05-20 22:26:53 +0900

    Time: 60 ms (9.97%), Space: 58.2 MB (34.63%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -9,23 +9,27 @@
 /**
  * @param {_Node} node
  * @return {_Node}
+ BF로 한다면 dfs든 bfs든 계속 카피하면서 neighbors에 추가하면 된다
+ 추가로 TLE방지를 위해 hashMap을 이용해 이미 카피한 노드의 값을 반환해주면 다시 만들지 않아도 됨
  */
-function dfs (node, map) {
-    if(node === null) return null;
-    if(map.has(node)) return map.get(node);
-
-    const copy = new Node(node.val);
-
-    map.set(node, copy)
-
-    for(const neighbor of node.neighbors) {
-        copy.neighbors.push(dfs(neighbor, map))
-    }
-
-    return copy
-}
-
 var cloneGraph = function(node) {
     const map = new Map();
-    return dfs(node, map)
+
+    function dfs(n) {
+        if(!n) return null;
+        if(map.has(n.val)) return map.get(n.val);
+
+        const copy = new _Node(n.val);
+        const originNeighbors = n.neighbors;
+        map.set(n.val, copy);
+
+        for(neighbor of originNeighbors) {
+            const copiedNeighbor = dfs(neighbor)
+            copy.neighbors.push(copiedNeighbor)
+        }
+
+        return copy;
+    }
+
+    return dfs(node)
 };
