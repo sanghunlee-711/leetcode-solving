9ed7e84e65b7572424eecef43ea6e607ee25b799
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-05-21 22:34:42 +0900

    Time: 80 ms (27.19%), Space: 62.6 MB (45.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -1,27 +1,24 @@
 /**
  * @param {character[][]} grid
  * @return {number}
- 방문처리를 꼭 한다
- 전체 회문을 돌면서 bfs를 통해 카운트를 한다.
- 회문돌때 1인 경우만 bfs를 한다
- bfs는 0을만나거나 Edge에 해당하는경우 멈춘다.
+ visited처리를 하면서 모든 좌표에 dfs를 진행하면
+ bfs가 진행되는 횟수만큼이 섬의 개수가 되겠죠?
+ visited처리시 방문했던곳을 다시 방문하지는 않을 것이므로
  */
  const directions = [
     [1,0],
-    [-1,0],
     [0,-1],
     [0,1],
- ]
+    [-1,0]
+ ];
 
 var numIslands = function(grid) {
     if(!grid || !grid.length) return 0;
 
     const rows = grid.length,
         cols = grid[0].length;
+    const visited = Array.from({length: rows}, () => Array.from({length:cols}, ()=> false));
     let count = 0;
-    const visited = Array.from({length: rows}, 
-                () => Array.from({length: cols}, 
-                () => false));
 
     function bfs(r,c) {
         const queue = [[r,c]];
@@ -29,14 +26,17 @@ var numIslands = function(grid) {
 
         while(queue.length) {
             const [row, col] = queue.shift();
-            for(const [dx,dy] of directions) {
-                const [nr, nc] = [row + dx, col + dy]
+            for(const [dx, dy] of directions) {
+                const nr = dx + row,
+                    nc = dy + col;
+
                 if(nr >= 0 && nr < rows &&
                     nc >= 0 && nc < cols &&
-                    grid[nr][nc] === '1' && !visited[nr][nc]) {
-                        visited[nr][nc] = true;
-                        queue.push([nr, nc])
-                    }
+                    grid[nr][nc]==='1' &&
+                    visited[nr][nc] === false) {
+                    visited[nr][nc] = true
+                    queue.push([nr,nc]);
+                }
             }
         }
     }
@@ -44,9 +44,10 @@ var numIslands = function(grid) {
     for(let r = 0; r < rows; r++) {
         for(let c = 0; c < cols; c++) {
             if(grid[r][c] === '1' && !visited[r][c]) {
-                count += 1;
+                count+=1;
                 bfs(r,c)
             }
+            
         }
     }
 
