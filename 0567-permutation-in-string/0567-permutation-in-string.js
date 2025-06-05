71cd0ae7741e01ab8758876ae449c658bd899527
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-06-05 14:04:50 +0900

    Time: 14 ms (69.25%), Space: 59.1 MB (57.43%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -3,39 +3,44 @@
  * @param {string} s2
  * @return {boolean}
  */
-/*
-Sliding Window
-
-*/
-
 var checkInclusion = function(s1, s2) {
-    //edge case
-    if (s1.length > s2.length) return false;
+    const needCount = {};
+    for(const char of s1) needCount[char] = (needCount[char] || 0) + 1;
 
-    const hash = {};
-    
-    //hash에 기록
-    s1.split("").forEach((el) => hash[el] = (hash[el] || 0)+1);
-    
-    let left = 0, right = 0, count = s1.length;
-    
+    const windowCount = {};
+    let matchCount = 0;
+    const target = Object.keys(needCount).length;
+
+    let left = 0, right = 0;
+
     while(right < s2.length) {
-        if(hash[s2[right]] > 0) count --;
-        //해당 글자 카운트를 줄이고 오른쪽으로 윈도우 확장
-        hash[s2[right]]--;
-        right++;
-        
-        //만약 다 카운팅 된 거라면 true반환
-        if(count === 0) return true;
-        
-        //윈도우크기가 카운트 해야할 s1과 동일 해졌다면 왼쪽에서 윈도우를 줄여야함
-        if(right - left === s1.length){
-            if(hash[s2[left]] >= 0) count++;
-            //윈도우에서 해당 요소를 빼고 있으므로 줄였던 카운트를 늘려줘야함
-            hash[s2[left]]++;
+        //윈도우 문자 추가 
+        const inChar = s2[right];
+
+        if(needCount[inChar] !== undefined) {
+            windowCount[inChar] = (windowCount[inChar] || 0) + 1;
+
+            if(windowCount[inChar] === needCount[inChar]) matchCount++;
+        }
+
+        //윈도우 크기 동일 시
+        if(right - left + 1 > s1.length) {
+            const outChar = s2[left];
+
+            if(needCount[outChar] !== undefined) {
+                if(windowCount[outChar] === needCount[outChar]) matchCount--;
+                windowCount[outChar]--;
+            }
             left++;
         }
+        
+
+        //permutation찾는 여부
+        if(target === matchCount) return true;
+
+        right++;//윈도우 확장
     }
-    
+
+
     return false;
 };
